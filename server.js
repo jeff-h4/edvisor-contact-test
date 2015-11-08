91787ecdc7ef15c59da192224f87a9bc579acfcf
Jeff H <jahuangi@yahoo.ca>
2015-11-07 16:41:44 -0800

    added .gitignore, minor edit to server.js

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname + '/public')).listen(8080);
+var serverPort = 8080;
+connect().use(serveStatic(__dirname + '/public')).listen(serverPort);
+
+console.log("Server can be reached at http://localhost:" + serverPort);
