bc55435e707272a617695bdebc87010be3258627
Jeff H <jahuangi@yahoo.ca>
2015-11-09 20:04:07 -0800

    got dynamic edit/display working. contact changes don't get permanently saved.

--- public/assets/js/controllers.js
+++ public/assets/js/controllers.js
@@ -3,14 +3,42 @@ var contactControllers = angular.module('contactControllers', []);
 contactControllers.controller('ContactListCtrl', ['$scope','$http',
     function ($scope,$http) {
       $http.get("assets/contacts.json").success(function(data) {
+        $scope.pageMode = "display";
+        //First pull data from the GET request
         $scope.contacts = data.contacts;
         for (var i = 0; i < data.contacts.length; i++ ){
           $scope.contacts[i].id = i;
         } 
+        //Now distribute this data to other vars
         $scope.selectedContact = $scope.contacts[0];
+        $scope.contactForm = {};
+        $scope.contactForm.phone = $scope.contacts[0].phone; 
+        $scope.contactForm.email = $scope.contacts[0].email; 
+        $scope.contactForm.address = $scope.contacts[0].address; 
         $scope.printSomething = function(id) {
           console.log("A contact with ID " + id + " was clicked");
           $scope.selectedContact = $scope.contacts[id];
+          $scope.phone_input = $scope.contacts[id].phone; 
+          $scope.email_input = $scope.contacts[id].email; 
+          $scope.address_input = $scope.contacts[id].address; 
+        };
+        $scope.edit = function(id) {
+          console.log("This is EDIT mode.");
+          $scope.pageMode = "edit";
+        };
+        $scope.save = function(id) {
+          console.log("This is DISPLAY mode.");
+          console.log($scope.selectedContact);
+          console.log($scope.contacts[id]);
+          console.log($scope.contactForm.phone);
+          console.log($scope.contactForm.email);
+          console.log($scope.contactForm.address);
+          $scope.pageMode = "display";
+          $scope.contacts[id].phone   = $scope.contactForm.phone;
+          $scope.contacts[id].email   = $scope.contactForm.email;
+          $scope.contacts[id].address = $scope.contactForm.address;
+          $scope.selectedContact = $scope.contacts[id];
+          //console.log($scope.contacts[id]);
         };
 
         $scope.message = "<strong>Jeff, we want to hire you.</strong>";
