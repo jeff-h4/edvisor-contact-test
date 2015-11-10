9274f0e093e6af38921a179cea062a8149f57efa
Jeff H <jahuangi@yahoo.ca>
2015-11-09 16:07:30 -0800

    got contact-detail HTML and related controller working to display the relevant view

--- public/assets/js/controllers.js
+++ public/assets/js/controllers.js
@@ -2,7 +2,7 @@ var contactControllers = angular.module('contactControllers', []);
 
 contactControllers.controller('ContactListCtrl', ['$scope','$http',
     function ($scope,$http) {
-      $http.get("assets/contacts/contacts.json").success(function(data) {
+      $http.get("assets/contacts.json").success(function(data) {
         $scope.contacts = data.contacts;
         console.log("Inside ContactListCtrl");
         console.log(data);
@@ -10,10 +10,27 @@ contactControllers.controller('ContactListCtrl', ['$scope','$http',
     }
 ]);
 
-contactControllers.controller('ContactDetailCtrl', ['$scope','$routeParams',
-    function ($scope,$routeParams) {
-      $scope.contactId = $routeParams.contactId;
+contactControllers.controller('ContactDetailCtrl', 
+    ['$scope','$routeParams','$http',
+    function ($scope,$routeParams, $http) {
       console.log("Inside ContactDetailCtrl");
+      //$scope.contactId = $routeParams.contactId;
+      $http.get("assets/contacts.json").success(function(data) {
+        $scope.contact = data.contacts[$routeParams.contactId];
+        console.log("Inside ContactListCtrl");
+        console.log(data);
+      });
+  
+      //$http.get('assets/' + $routeParams.contactId + '.json').
+      //  success(function(data) {
+      //    $scope.contact = data;
+      //    console.log("ContactDetailCtrl: contact is:");
+      //    console.log(data);
+      //});
+
+      //console.log($scope);
+      //console.log($routeParams);
+      //$scope.selectedContact = $scope.contacts[$routeParams.contactId];
     }
 ]);
 
